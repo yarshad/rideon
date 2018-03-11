57ef3c18e05d5ece7a2eee76dc095442bd2e770c
yaser <yarshad22@gmail.com>
2018-03-10 22:18:37 -0500

    updates

--- app/src/main/java/com/foxhound/rideon/MainActivity.java
+++ app/src/main/java/com/foxhound/rideon/MainActivity.java
@@ -3,12 +3,14 @@ package com.foxhound.rideon;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSignIn, btnRegister;
 
     @Override
     protected void attachBaseContext(Context newBase){
@@ -27,5 +29,10 @@ public class MainActivity extends AppCompatActivity {
                         .build());
 
         setContentView(R.layout.activity_main);
+
+        //Init view
+
+        btnRegister = findViewById(R.id.btnRegister);
+        btnSignIn = findViewById(R.id.btnSignIn);
     }
 }
