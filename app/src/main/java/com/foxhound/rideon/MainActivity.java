b224ac8586077170dc7f02ec8f25eb96ba4becaa
yaser <yarshad22@gmail.com>
2018-03-11 04:25:39 -0400

    location & google maps

--- app/src/main/java/com/foxhound/rideon/MainActivity.java
+++ app/src/main/java/com/foxhound/rideon/MainActivity.java
@@ -1,14 +1,13 @@
 package com.foxhound.rideon;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
+ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +23,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import dmax.dialog.SpotsDialog;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -108,7 +108,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
 
-                dialogInterface.cancel();
+                dialogInterface.dismiss();
+
+                //set disable button sign in if is processing
+
+                btnSignIn.setEnabled(false);
 
                 //Validation
 
@@ -123,6 +127,10 @@ public class MainActivity extends AppCompatActivity {
                 }
 
 
+                final AlertDialog waitingDialog = new SpotsDialog(MainActivity.this);
+                waitingDialog.show();
+
+
                 //Login
 
                 auth.signInWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString())
@@ -130,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(AuthResult authResult) {
 
+                        waitingDialog.dismiss();
                         startActivity(new Intent(MainActivity.this,Welcome.class));
                         finish();
 
@@ -138,8 +147,10 @@ public class MainActivity extends AppCompatActivity {
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-
+                        waitingDialog.dismiss();
                         Snackbar.make(rootLayout,"Login failed" + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+
+                        btnSignIn.setEnabled(true);
                      }
                 });
 
